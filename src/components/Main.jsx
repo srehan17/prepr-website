f27d4f17bb81ff3f54c75d3a59b7d1dcd269e53f
Sarah Rehan <sarahahmad17@gmail.com>
2024-02-15 23:46:43 -0500

    Added tabs, search box and styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const image = require('../images/image2.jpeg')
+const searchIcon = require('../images/search.png')
 
 const Main = () => {
+
     return (
         <div style={{ backgroundImage: `url(${image})`, height: '550px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <h1 style={{ fontSize: '3rem' }}>Explore Your Interests</h1>
@@ -12,7 +14,10 @@ const Main = () => {
             <div className='small-font' style={{ marginBottom: '20px' }}>
                 Feel free to add or remove topics to customize your experience!
             </div>
-            <input style={{ width: '50%', height: '60px' }} />
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: '40px', border: '1px solid gray', borderRadius: '15px' }}>
+                <img src={searchIcon} alt="search icon" style={{ width: '30px', padding: '7px', backgroundColor: 'white', cursor: 'pointer' }} />
+                <input style={{ width: '300px', height: '40px', outline: 'none', fontSize: '1.3rem', paddingLeft: '10px' }} />
+            </div>
             <div style={{ fontSize: '2rem', fontWeight: 'bold', width: '500px', marginTop: '20px', textAlign: 'center' }}>Click an interest to narrow your recommendations.</div>
             <div className='tabs'>
                 <div className='tab'>Prototyping</div>
@@ -23,7 +28,6 @@ const Main = () => {
                 <div className='tab'>Skills</div>
                 <div className='tab'>Budget Management</div>
                 <div className='tab'>Create SVG Graphics</div>
-                <div className='tab'></div>
                 <div className='tab'>Journaling</div>
                 <div className='tab'>Food Manufacturing</div>
             </div>
