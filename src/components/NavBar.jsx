42fc96430b87d9f6ba4a126cf26464ae3aac57e2
Sarah Rehan <sarahahmad17@gmail.com>
2024-02-20 16:21:40 -0500

    Updated dropdown hover

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,12 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const NavBar = () => {
+    const [showDropdown, setShowDropdown] = useState(false)
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown)
+    }
+
     const hamburgerRef = useRef(null)
     const navMenuRef = useRef(null)
 
@@ -26,12 +32,18 @@ const NavBar = () => {
             <ul className='nav-bar' ref={navMenuRef}>
 
                 <li className="nav-item">Explore</li>
-                <li style={{ position: 'relative' }}> <div style={{ display: 'flex', flexDirection: "column", alignItems: 'center', }} className="nav-item">
+                <li style={{ position: 'relative' }}> <div style={{ display: 'flex', flexDirection: "column", alignItems: 'center', }} className="nav-item dashboard-nav"
+                    onMouseEnter={() => setShowDropdown(true)}
+                // onMouseLeave={() => setShowDropdown(false)}
+                >
                     <span className="dropdown" >Dashboard</span>
-                    <div className="hide" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '5px', position: 'absolute', padding: '10px', backgroundColor: "#fff", marginTop: '25px', display: 'none' }}>
-                        <img src="https://d3f06rtlkr354b.cloudfront.net/public/front/img/user.png" style={{ width: '20px', height: 'auto' }} /><span style={{ fontSize: '0.9rem' }}>User Dashboard</span></div>
+                    {showDropdown && (<div className="hide" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '5px', position: 'absolute', padding: '10px', backgroundColor: "#fff", marginTop: '25px', border: '0.1px solid gray' }}
+                        onMouseEnter={() => setShowDropdown(true)}
+                        onMouseLeave={() => setShowDropdown(false)}
+                    >
+                        <img src="https://d3f06rtlkr354b.cloudfront.net/public/front/img/user.png" style={{ width: '25px', height: 'auto' }} /><span style={{ fontSize: '0.9rem' }}>User Dashboard</span></div>)}
                 </div>
-                </li>
+                </li >
                 <li className="nav-item">My Challenges</li>
                 <li className="nav-item">My Labs</li>
                 <li className="nav-item">My Projects</li>
@@ -41,13 +53,13 @@ const NavBar = () => {
                 <li className="nav-item"><img src="https://preprlabs.org/assets/images/header/help-icon.svg" /></li>
                 <li className="nav-item"><input type="text" className="nav-searchbar" placeholder='Search...' /></li>
                 <li className="nav-item"><img style={{ width: '50px' }} src="https://prepr-preprlabs-prod-bucket.s3.ca-central-1.amazonaws.com/front/img/default-user.png" /></li>
-            </ul>
+            </ul >
             <div className="hamburger" ref={hamburgerRef}>
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span>
             </div>
-        </div>
+        </div >
     )
 }
 
